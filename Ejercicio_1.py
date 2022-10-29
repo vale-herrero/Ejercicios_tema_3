7b7ba84a5187f0f0975d6cadd4c704641d41d604
vale-herrero <vale4134@gmail.com>
2022-10-29 16:50:48 +0200

    intento ej1

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,1 +1,21 @@
+def move(disks, tubo_inicial=1, auxiliary=2, tubo_final=3):
 
+	if disks > 0:
+
+		# move `n-1` discs from source to auxiliary using the target
+		# as an intermediate pole
+		move(disks - 1, tubo_inicial, tubo_final, auxiliary)
+
+		# move one disc from source to target
+		print(f'Move disk {disks} from {tubo_inicial} —> {tubo_final}')
+
+		# move `n-1` discs from auxiliary to target using the source
+		# as an intermediate pole
+		move(disks - 1, auxiliary, tubo_inicial, tubo_final)
+
+
+# Tower of Hanoi Problem
+if __name__ == '__main__':
+
+	n = 10
+	move(n)
