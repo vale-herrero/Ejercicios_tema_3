d1a3189514a4b25e1bde0c6cf95b05538a570387
vale-herrero <vale4134@gmail.com>
2022-10-30 16:39:18 +0100

    se avanzo ej2

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -14,13 +14,26 @@ matriz=[[1,2,3],[3,5,6],[6,7,8]]
 #print(matriz)
 
 def det_iterativa(matriz):
+    "forma iterativa"
     matriz=matriz*2
     matriz.pop(-1)
+    determinante=0
     for i in range(0,3):
-        termino1=matriz[i][0]*matriz[i-1][1]*matriz[i-2][2]
-        termino2=matriz[i][0]*matriz[i+1][1]*matriz[i+2][2]
-    determinante= termino1-termino2
+        sumas=matriz[i][0]*matriz[i+1][1]*matriz[i+2][2]
+        restas=matriz[i][2]*matriz[i+1][1]*matriz[i+2][0]
+        determinante= determinante + (sumas-restas)
     print(determinante)
 
 det_iterativa(matriz)
 
+def det_recursiva(matriz):
+    matriz=matriz*2
+    matriz.pop(-1)
+    i=0
+    sumas=matriz[i][0]*matriz[i+1][1]*matriz[i+2][2]
+    det_iterativa(sumas)
+    restas=matriz[i][2]*matriz[i+1][1]*matriz[i+2][0]
+    i+=1
+    det_recursiva(restas)
+    print(det_recursiva(sumas)-det_recursiva(restas))
+det_recursiva(matriz)
