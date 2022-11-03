7e30f9a96cdd7046b52bc94ace31cbcdf12d1029
vale-herrero <vale4134@gmail.com>
2022-11-03 13:33:14 +0100

    se intento ej2

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,17 +1,6 @@
-
-"determinante matriz iterativa"
 from sympy import Determinant
 
-
 matriz=[[1,2,3],[3,5,6],[6,7,8]]
-"""1,2,3
-   3,5,6
-   6,7,8
-   
-   (1*5*8)+(3*7*3)+(2*6*6)-(()) """
-#matriz=matriz*2
-#matriz.pop(-1)
-#print(matriz)
 
 def det_iterativa(matriz):
     "forma iterativa"
