161fcb45aa478f83aab5a8693ae8e96fa01c29eb
vale-herrero <vale4134@gmail.com>
2022-11-06 18:06:58 +0100

    termine ej1

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,14 +1,10 @@
 
+def TorreDeHanoi(Discos, agujaFuente, agujaDestino, agujaAuxiliar):
+	if Discos == 0: 
+		return None
+	elif Discos > 0:
+		TorreDeHanoi(Discos-1, agujaFuente, agujaAuxiliar, agujaDestino)
+		print("Mover disco: ", Discos, "desde la aguja: ", agujaFuente, "hasta la aguja: ", agujaDestino) 
 
-def move(discos, tubo_inicial=1, auxiliar=2, tubo_final=3):
 
-	if discos > 0:
-
-		move(discos - 1, tubo_inicial, tubo_final, auxiliar)
-		print(f'Move disk {discos} from {tubo_inicial} —> {tubo_final}')
-		move(discos - 1, auxiliar, tubo_inicial, tubo_final)
-
-if __name__ == '__main__':
-
-	n = 74
-	move(n)
+TorreDeHanoi(74,1,2,3)
